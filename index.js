846312418aa30d4d46160c33303fa3b0cd520f5a
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-15 08:49:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// const body = document.getElementsByTagName("body")[0];
+// const secondChild = body.querySelector("main");
+// body.removeChild(secondChild);
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
